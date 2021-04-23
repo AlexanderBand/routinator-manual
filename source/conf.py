c7d80401d4b811f7ad4d007eafa1f96bd9b7a89b
Alex Band <alex@nlnetlabs.nl>
2021-04-23 15:07:19 +0200

    conf.py tweaks

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,6 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['../templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -77,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = 'img/Routinator_Wordmark.png'
 html_favicon = 'img/favicon.ico'
@@ -95,7 +94,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['resources']
+html_static_path = ["resources"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
